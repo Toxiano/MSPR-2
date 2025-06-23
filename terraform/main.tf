32ab1f4aef91cb103522dbcd4befb662e2cf8615
Alexis PARSY <134281939+Alexis869@users.noreply.github.com>
2025-06-23 15:33:51 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,12 @@ resource "google_compute_instance" "vm_from_packer_image" {
 
   metadata_startup_script = <<-EOT
     #!/bin/bash
+
+    # Installation de l'agent Ops (logs + metrics)
+    curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+    sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+    systemctl restart google-cloud-ops-agent
+
     echo "Instance from Packer image ready."
   EOT
 
